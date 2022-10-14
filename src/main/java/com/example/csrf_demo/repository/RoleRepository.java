f3afe81c46d5362e2907214cd447f88e4e579ca1
Junayed Rahman <junayed.rahman@g.bracu.ac.bd>
2022-10-14 14:32:53 +0600

    Added bootstrap dependency

--- src/main/java/com/example/csrf_demo/repository/RoleRepository.java
+++ src/main/java/com/example/csrf_demo/repository/RoleRepository.java
@@ -1,4 +1,7 @@
 package com.example.csrf_demo.repository;
 
-public interface RoleRepository {
+import com.example.csrf_demo.entity.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
 }

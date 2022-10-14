23623313073f0ff082d4ed365b18500da90a70cc
Junayed Rahman <junayed.rahman@g.bracu.ac.bd>
2022-10-14 11:47:10 +0600

    removed auto generated id of Roles

--- src/main/java/com/example/csrf_demo/entity/RoleEntity.java
+++ src/main/java/com/example/csrf_demo/entity/RoleEntity.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 @Table(name = "roles")
 public class RoleEntity extends BaseEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "name")
     private String name;
